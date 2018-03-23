d97b8f75d39773cdc5141797b37ebba7a9f01798
Van Olivares <volivaresh@gmail.com>
2018-03-22 23:58:07 -0300

    Se comienza a utilizar CLI y se testean argumentos 1,2,5,6

--- code/fake-warehouse/src/main/java/cl/emagina/curso/fakewarehouse/FakeWarehouseApp.java
+++ code/fake-warehouse/src/main/java/cl/emagina/curso/fakewarehouse/FakeWarehouseApp.java
@@ -4,12 +4,23 @@ import java.io.*;
 import java.io.File;
 import org.apache.commons.cli.*;
 
+
+/**
+* Recibe los argumentos por linea de comando, consulta los archivos, extrae
+* la data necesaria y carga la base de datos con la informacion requerida 
+*/
 public class FakeWarehouseApp{
+	
+	private CommandLineParser parser=null;
+	private CommandLine cmdLine=null;
+	private OutputStream output = null;
+	private File file=null;
 	
 	public enum InformationSource{
 		INVENT,CARTOLA,PLANGER;
 	} 	
 	
+
 	public static void main (String[] args){
 		
 		FakeWarehouseApp fwa = new FakeWarehouseApp();
@@ -18,9 +29,10 @@ public class FakeWarehouseApp{
 	
 	public boolean init(String[] args){
 		
-		CommandLineParser parser=null;
-		CommandLine cmdLine=null;
-		OutputStream output = null;
+
+
+		//Verificando parametros
+		verifArgs(args);
 		
 		//Configurtamos las opciones de entrada
 		Options opt = new Options();
@@ -53,31 +65,42 @@ public class FakeWarehouseApp{
 		//Aqui ocurre la magia...
 		
 		
-		
-		
-		//Verificando parametros
-		verifArgs(args);
-		
 		return true;
 	}
 	
 	private void verifArgs(String[] args){
 		
+		//Verifica que esten presentes todos los argumentos
 		if(args.length!=7){
 			throw new IllegalArgumentException("Se debe ingresar los 7 parametros requeridos");
 		}
 		
-		//VErifica parametro 1 sea valido
+		//Verifica parametro 1 sea valido
 		if(args[0]!=InformationSource.INVENT.toString() && args[0]!=InformationSource.CARTOLA.toString() && args[0]!=InformationSource.PLANGER.toString() ){
 			
 			throw new IllegalArgumentException("El primer parametro debe ser INVENT, CARTOLA o PLANGER");
 		}
 		
-		//Verifica parametro 2 sea valido
-		File file = new File(args[1]);
-		if(!file.isFile()){
+		//Verifica parametro 2 sea valido, se busca un archivo que exista.
+		file = new File(args[1]);
+		System.out.println(file.getPath());
+		if(!file.isFile()||!file.exists()){
 			throw new IllegalArgumentException("Archivo no existe");
 		}
+		
+		//Verifica parametro 6 sea valido, arroja excepcion si argumento no es Integer.
+		try{
+			Integer.valueOf(args[5]);
+		}catch(NumberFormatException e){
+			throw new NumberFormatException("Archivo no existe");
+		}
+		
+		//Verifica parametro 6 sea valido, Integer debe ser mayor que 0 y Menor que 65535.
+		if(Integer.valueOf(args[5])<0 || Integer.valueOf(args[5]) > 65535){
+			throw new IllegalArgumentException("Puerto debe ser mayor que 0 y menor o igual que 65535");
+		}
+		
+		
 	}
 	
 
--- code/fake-warehouse/src/test/java/cl/emagina/curso/fakewarehouse/TestFakeWarehouseApp.java
+++ code/fake-warehouse/src/test/java/cl/emagina/curso/fakewarehouse/TestFakeWarehouseApp.java
@@ -3,6 +3,7 @@ package cl.emagina.curso.fakewarehouse;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.io.File;
 
 
 
@@ -11,9 +12,10 @@ public class TestFakeWarehouseApp{
 	private String ruta="C:\\Users\\Owner\\Documents\\curso ybl 2018\\fake-warehouse\\test.txt";
 	private String valid_ip="192.168.2.34";
 	private String valid_port="34567";
+	private File arch=null;
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void deberia_retornar_IllegalArgumentException_6_params(){
+	public void deberia_Retornar_IllegalArgumentException_1_Params_Cuando_Espera_7(){
 		
 		//Cuando
 		String[] args = {"uno"};
@@ -24,7 +26,7 @@ public class TestFakeWarehouseApp{
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void deberia_retornar_IllegalArgumentException_without_params(){
+	public void deberia_Retornar_IllegalArgumentException_Si_No_Recibe_Params(){
 		
 		//Cuando
 		String[] args = {""};
@@ -35,7 +37,7 @@ public class TestFakeWarehouseApp{
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void deberia_retornar_IllegalArgumentException_8_params(){
+	public void deberia_Retornar_IllegalArgumentException_Si_Recibe_8_Params_Cuando_Espera_7(){
 		
 		//Cuando
 		String[] args = {"uno","uno","2","3","4","5","6","7"};
@@ -45,6 +47,7 @@ public class TestFakeWarehouseApp{
 		mn.init(args);
 	}
 	
+	/*
 	@Test
 	public void correct_number_of_params(){
 		
@@ -54,10 +57,10 @@ public class TestFakeWarehouseApp{
 		FakeWarehouseApp mn=new FakeWarehouseApp();
 		
 		assertTrue(mn.init(args));
-	}
+	}*/
 
 	@Test (expected=IllegalArgumentException.class)
-	public void retorna_IllegalArgumentException_si_param_1_no_es_INVENT_o_CARTOLA_o_PLANGER(){
+	public void retorna_IllegalArgumentException_Si_Param_1_No_Es_INVENT_o_CARTOLA_o_PLANGER(){
 		//Cuando
 		String[] args = {"uno","2","3","4","5","6","7"};
 		
@@ -66,6 +69,7 @@ public class TestFakeWarehouseApp{
 		mn.init(args);
 	}
 	
+	/*
 	@Test
 	public void retorna_TRUE_si_param_1_es_INVENT(){
 		//Cuando
@@ -95,17 +99,107 @@ public class TestFakeWarehouseApp{
 		
 		assertTrue(mn.init(args));
 	}
-	
+	*/
 	@Test (expected = IllegalArgumentException.class)
-	public void retorna_illegalargumentexception_si_param_2_no_es_archivo(){
+	public void retorna_IllegalArgumentException_Si_Param_2_No_Es_Archivo(){
+		
 		//Dado
-		String[] args = {"PLANGER","2","3","4","5","6","7"};
+		try{
+			arch=File.createTempFile("test_",null);
+			//System.out.println(arch.getPath());
+		}catch(Exception e){
+			System.out.println("Excepcion :"+e);
+		}
+		
+		String[] args = {"PLANGER","bla.por","3","4","5","6","7"};
+		
+		FakeWarehouseApp mn=new FakeWarehouseApp();
+		mn.init(args);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void retorna_IllegalArgumentException_SI_Param_5_No_ES_IP_Valido(){
+		
+		//Dado
+		try{
+			arch=File.createTempFile("test_",null);
+			//System.out.println(arch.getPath());
+		}catch(Exception e){
+			System.out.println("Excepcion :"+e);
+		}
+		
+		String[] args = {"PLANGER",arch.getPath(),"3","4","5","6","7"};
 		
 		FakeWarehouseApp mn=new FakeWarehouseApp();
 		
-		//mn.init(args);
+		mn.init(args);
 		
-		assertTrue(mn.init(args));
+		
 	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void retorna_IllegalArgumentException_Si_Param_6_No_ES_Un_Puerto_Valido_Al_Recibir_String(){
+
+		//Dado
+		try{
+			arch=File.createTempFile("test_",null);
+			//System.out.println(arch.getPath());
+		}catch(Exception e){
+			System.out.println("Excepcion :"+e);
+		}
+		
+		String[] args = {"PLANGER",arch.getPath(),"3","4","5","bla","7"};
+		
+		FakeWarehouseApp mn=new FakeWarehouseApp();
+		
+		mn.init(args);
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void retorna_IllegalArgumentException_Si_Param_6_Es_Menor_Que_0(){
+
+		//Dado
+		try{
+			arch=File.createTempFile("test_",null);
+			//System.out.println(arch.getPath());
+		}catch(Exception e){
+			System.out.println("Excepcion :"+e);
+		}
+		
+		String[] args = {"PLANGER",arch.getPath(),"3","4","5","-1","7"};
+		
+		FakeWarehouseApp mn=new FakeWarehouseApp();
+		
+		mn.init(args);
+		
+	}	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void retorna_IllegalArgumentException_Si_Param_6_Es_Mayor_Que_65535(){
+
+		//Dado
+		try{
+			arch=File.createTempFile("test_",null);
+			//System.out.println(arch.getPath());
+		}catch(Exception e){
+			System.out.println("Excepcion :"+e);
+		}
+		
+		String[] args = {"PLANGER",arch.getPath(),"3","4","5","65536","7"};
+		
+		FakeWarehouseApp mn=new FakeWarehouseApp();
+		
+		mn.init(args);
+		
+	}		
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void retorna_IllegalArgumentException_Si_Conexion_a_BD_No_Cumple_Con_Formato(){}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void retorna_IllegalArgumentException_Si_Tipo_de_Archivo_No_Es_Coherente_Con_Parametro_1(){}
+	
+	
 
 }
